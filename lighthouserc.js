acec84a9ecf82014c2dce378da4207d45a2700d5
LBROCHARD <lewis.brochard@gmail.com>
2025-07-09 15:07:11 +0200

    feat: updated lighthouse CI configuration

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            "startServerCommand": "pnpm run preview",
+            "startServerCommand": "npm run start",
             "startServerReadyPattern": "Local:",
             "url": [
                 "http://localhost:3000/",
