f96d986e40b75c8ee74c33b365b6fbf815f2218b
LBROCHARD <lewis.brochard@gmail.com>
2025-08-14 17:30:11 +0200

    feat: protecting API from spam

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { PagesController } from './modules/pages/pages.controller';
 import { PagesService } from './modules/pages/pages.service';
 import { PagesModule } from './modules/pages/pages.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -19,9 +21,22 @@ import { ConfigModule } from '@nestjs/config';
     PagesModule, 
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
   controllers: [AppController, PagesController],
-  providers: [AppService, PagesService],
+  providers: [
+    AppService, 
+    PagesService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
